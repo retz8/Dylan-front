092daa72f9759781321446cdc53298fac8cf8c9c
retz8 <ekkicb71@gmail.com>
2024-04-12 18:12:48 -0400

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* header */}
+
+        {/* page.js */}
+        <main>{children}</main>
+
+        {/* footer */}
+      </body>
     </html>
   );
 }
